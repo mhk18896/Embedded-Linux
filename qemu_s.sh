91642e076b5a3ae54b9177e0fd1ab76cace25fa5
s314424 <s314424@studenti.polito.it>
2024-12-16 15:30:53 +0000

    all

--- qemu_s.sh
+++ qemu_s.sh
@@ -1,8 +1,10 @@
-qemu-system-aarch64  \
+ qemu-system-aarch64 \
 -machine  virt  \
 -cpu cortex-a57 -smp 4  \
--kernel  /home/m-hamza-khan/Desktop/project_os/build/tmp/deploy/images/qemuarm64/Image  \
--drive file=/home/m-hamza-khan/Desktop/project_os/build/tmp/deploy/images/qemuarm64/core-image-minimal-qemuarm64.rootfs.ext4,format=raw  \
+-m 4096 \
+-kernel  /home/m-hamza-khan/Desktop/project_os/build-cortexa57/tmp/deploy/images/qemuarm64/Image  \
+-drive file=/home/m-hamza-khan/Desktop/project_os/build-cortexa57/tmp/deploy/images/qemuarm64/my-image-qemuarm64.rootfs.ext4,format=raw  \
 -append "root=/dev/vda " \
--nographic \
-
+-device pci-echodev \
+-nic user,hostfwd=tcp::2222-:22 \
+-nographic
